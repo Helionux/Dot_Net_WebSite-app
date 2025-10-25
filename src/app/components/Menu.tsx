be54cc0a108d4370ce039ba7363ad475e141aa8b
unknown <deouleallahh@gmail.com>
2025-10-25 06:42:39 -0700

    update menu

--- src/app/components/Menu.tsx
+++ src/app/components/Menu.tsx
@@ -13,92 +13,87 @@ export default function Menu() {
             </div>
 
             <div className=" max-w-7xl flex flex-nowrap gap-3 ml-5 overflow-auto box-border">
-                <div className="mt-5 w-[190px]">
+                <div className="mt-5 w-[190px] font-grotesk">
                     <div className="max-w-sm bg-[#512BD4]  h-[180px] rounded-2xl mt-20  p-6">
-                        <p className="w-full h-full justify-center items-center flex font-['Space_Grotesk'] font-bold text-[20px] text-[#FFFFFF] ">
+                        <p className="w-full h-full justify-center items-center flex font-bold text-[20px] text-[#FFFFFF] ">
                         Key
-                        <span className="font-['Space_Grotesk'] text-[#575362]">
+                        <span className=" text-[#ffffffa4]">
                             note
                         </span>
                         </p>
                     </div>
-                    <h4 className="font-bold text-[15px] mt-0.5">1. Keynote</h4>
-                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal ">
+                    <h4 className="font-bold text-[15px] mt-4">1. Keynote</h4>
+                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal mt-2">
                         Doriane Mangamtcheuth Analyste M365 | Power Platform | Microsoft Dynamics 365
                     </p>
                 </div>
-                <div className="mt-5 w-[190px]">
+                <div className="mt-5 w-[190px] font-grotesk">
                     <div className="max-w-sm bg-[#0A855F]  h-[180px] rounded-2xl mt-25 shadow-lg p-6">
-                        <p className="w-full h-full justify-center items-center flex font-['Space_Grotesk'] font-bold text-[20px] text-[#FFFFFF] ">
-                        Key
-                        <span className="font-['Space_Grotesk'] text-[#575362]">
-                            note
+                        <p className="w-full h-full justify-center items-center flex  font-bold text-[20px] text-[#FFFFFF] ">
+                        Conf
+                        <span className=" text-[#ffffffa4]">
+                            ernce
                         </span>
                         </p>
                     </div>
-                    <h4 className="font-bold text-[15px] mt-0.5">1. Keynote</h4>
-                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal ">
+                    <h4 className="font-bold text-[15px] mt-4">2. Keynote</h4>
+                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal mt-2">
                         Doriane Mangamtcheuth Analyste M365 | Power Platform | Microsoft Dynamics 365
                     </p>
                 </div>
-                <div className="mt-5 w-[190px]">
+                <div className="mt-5 w-[190px] font-grotesk">
                     <div className="max-w-sm bg-[#E1A325]  h-[180px] rounded-2xl mt-20  p-6">
-                        <p className="w-full h-full justify-center items-center flex font-['Space_Grotesk'] font-bold text-[20px] text-[#FFFFFF] ">
-                        Key
-                        <span className="font-['Space_Grotesk'] text-[#575362]">
-                            note
+                        <p className="w-full h-full justify-center items-center flex  font-bold text-[20px] text-[#2D2006] ">
+                        Conf
+                        <span className=" text-[#2d20068b]">
+                            ernce
                         </span>
                         </p>
                     </div>
-                    <h4 className="font-bold text-[15px] mt-0.5">1. Keynote</h4>
-                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal ">
+                    <h4 className="font-bold text-[15px] mt-4">3. Copilot: prise en main de copilot sur Microsoft 365</h4>
+                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal mt-2">
                         Doriane Mangamtcheuth Analyste M365 | Power Platform | Microsoft Dynamics 365
                     </p>
                 </div>
-                <div className="mt-5 w-[190px]">
+                <div className="mt-5 w-[190px] font-grotesk">
                     <div className="max-w-sm bg-[#512BD4]  h-[180px] rounded-2xl mt-25 shadow-lg p-6">
-                        <p className="w-full h-full justify-center items-center flex font-['Space_Grotesk'] font-bold text-[20px] text-[#FFFFFF] ">
-                        Key
-                        <span className="font-['Space_Grotesk'] text-[#575362]">
-                            note
-                        </span>
+                        <p className="w-full h-full justify-center items-center flex font-bold text-[20px] text-[#FFFFFF] ">
+                        Sponsor<span className=" text-[#ffffffa4] ml-1"> Time</span>
                         </p>
                     </div>
-                    <h4 className="font-bold text-[15px] mt-0.5">1. Keynote</h4>
-                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal ">
+                    <h4 className="font-bold text-[15px] mt-4 ">4. Sponsor Time - ITIA</h4>
+                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal mt-2">
                         Doriane Mangamtcheuth Analyste M365 | Power Platform | Microsoft Dynamics 365
                     </p>
                 </div>
-                <div className="mt-5 w-[190px]">
+                <div className="mt-5 w-[190px] font-grotesk">
                     <div className="max-w-sm bg-[#E1A325]  h-[180px] rounded-2xl mt-20  p-6">
-                        <p className="w-full h-full justify-center items-center flex font-['Space_Grotesk'] font-bold text-[20px] text-[#FFFFFF] ">
+                        <p className="w-full h-full justify-center items-center flex font-bold text-[20px] text-[#2D2006] ">
                         Key
-                        <span className="font-['Space_Grotesk'] text-[#575362]">
+                        <span className=" text-[#2d20068b]">
                             note
                         </span>
                         </p>
                     </div>
-                    <h4 className="font-bold text-[15px] mt-0.5">1. Keynote</h4>
-                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal ">
+                    <h4 className="font-bold text-[15px] mt-4">5. Deploy .NET Aspire app to Kubernetes using Azure DevOps pipelines</h4>
+                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] mt-4 tracking-normal">
                         Doriane Mangamtcheuth Analyste M365 | Power Platform | Microsoft Dynamics 365
                     </p>
                 </div>
-                <div className="mt-5 w-[190px]">
+                <div className="mt-5 w-[190px] font-grotesk">
                     <div className="max-w-sm bg-[#0A855F]  h-[180px] rounded-2xl mt-25 shadow-lg p-6">
-                        <p className="w-full h-full justify-center items-center flex font-['Space_Grotesk'] font-bold text-[20px] text-[#FFFFFF] ">
+                        <p className="w-full h-full justify-center items-center flex font-bold text-[20px] text-[#FFFFFF] ">
                         Key
-                        <span className="font-['Space_Grotesk'] text-[#575362]">
+                        <span className=" text-[#ffffffa4]">
                             note
                         </span>
                         </p>
                     </div>
-                    <h4 className="font-bold text-[15px] mt-0.5">1. Keynote</h4>
-                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal ">
+                    <h4 className="font-bold text-[15px] mt-4">1. Keynote</h4>
+                    <p className="font-[Lexend] font-light text-[14px] leading-[140%] tracking-normal mt-2">
                         Doriane Mangamtcheuth Analyste M365 | Power Platform | Microsoft Dynamics 365
                     </p>
                 </div>
-                
-                
             </div>
         </div>
     )

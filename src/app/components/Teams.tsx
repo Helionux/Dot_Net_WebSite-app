7dd84b1c8cbe3fd54b7bf486ad3f405a871e7403
unknown <deouleallahh@gmail.com>
2025-10-28 12:44:38 -0700

    update

--- src/app/components/Teams.tsx
+++ src/app/components/Teams.tsx
@@ -7,7 +7,7 @@ export default function Teams() {
         <div className="w-full bg-[#1F143B] rounded-[44px]">
             <div className="max-w-7xl mx-auto py-20 px-8">
                 <div className="max-w-[353px]">
-                    <h2 className="font-grotesk text-[36px] text-[#FFFFFF] font-bold leading-[110%] tracking-[-8]">Renowned experts <span className="text-[#8B6BF8]">to inspire you</span></h2>
+                    <h2 className="font-grotesk text-[36px] text-[#FFFFFF] font-bold ">Renowned experts <span className="text-[#8B6BF8]">to inspire you</span></h2>
                 </div>
 
                 <div className=" bg-amber-100">

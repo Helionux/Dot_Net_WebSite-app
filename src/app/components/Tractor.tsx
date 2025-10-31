5f9653971ded021d23133b5fbf1009811bc5bd2f
unknown <deouleallahh@gmail.com>
2025-10-30 23:30:59 -0700

    about

--- src/app/components/Tractor.tsx
+++ src/app/components/Tractor.tsx
@@ -1,37 +1,33 @@
-// import Image from "next/image";
+import Image from "next/image"
 
-// export default function Expert() {
-//   return (
-//     <div className="m-3 w-wull h-440 bg-[#1F143B] mt-10 rounded-2xl">
-//       <div className="max-w-6xl mx-auto">
-//         <div className="font-['Space_Grotesk'] font-bold text-[25px] leading-[110%] tracking-[-0.08em]">
-//           <span className="text-[#FFFFFF]">Renowned experts to</span> <br />
-//           <span className="text-[#8B6BF8]">inspire you</span>
-//         </div>
-       
-//       </div>
-//     </div>
-//   );
-// }
-
-// <div className="w-[292px] flex flex-col bg-[#FFFFFF] rounded-[13px] shadow-lg ">
-//                                     <div className=" rounded-2xl pl-5 -mt-[50px]">
-//                                         <Image
-//                                         className=" w-[100px] h-[100px] object-cover border-5 border-[#1F143B] rounded-2xl"
-//                                         src="/expert-3.png"
-//                                         alt="Hero Image"
-//                                         width={100}
-//                                         height={100}
-//                                         />
-//                                     </div>
-//                                     <div className="p-5">
-//                                         <h4 className=" font-['Space_Grotestk'] text-[20px] text-[#190649] font-bold">Georges FOTSO</h4>
-//                                         <p className="font-['Lexend']  text-[#404040] font-light text-[14px] border-b border-[#1F143B] pb-3">Principal Solution Architect</p>
-//                                         <ul className=" text-[15px] px-3 list-inside p-3 list-disc">
-//                                             <li>15+ years of .NET experience</li>
-//                                             <li>Microsoft MVP for 8 years</li>
-//                                             <li>Author of 3 books on software architecture</li>
-//                                         </ul>
-//                                         <p className="ml-2 font-['Lexend'] border-t border-[#1F143B] pt-3 text-[#404040] font-light text-[16px]">Session: &quot;Revolutionize your applications with .NET 9 and AI&quot;</p>
-//                                     </div>
-//                             </div>
+export default function About() {
+    return (
+      <div className="px-2.5">
+            <div className="max-w-7xl max-auto">
+                <div className=" p-8">
+                    <div className="max-w-[403px]">
+                        <h1 className="text-[44px] font-bold ">They were there <span className="text-[#512BD4]"> last year.</span></h1>
+                    </div>
+                    <div className=" mt-6 grid grid-cols-4 gap-6">
+                        <p>
+                            &quot;An incredible experience! The workshops on .NET 8 and applied AI really helped me improve my skills. Well done to the team for the flawless organization.&quot;
+                        </p>
+                        <p>
+                            &quot;It was my first time participating and I loved it. I got to meet experts I already follow online and ask them all my questions. I&apos;m leaving feeling super motivated!&quot;
+                        </p>
+                        <p>
+                            &quot;The sessions on Azure and hybrid cloud were very practical and applicable to our projects. I highly recommend this conference to anyone who wants to advance in the Microsoft ecosystem.&quot;
+                        </p>
+                        <p>
+                            &quot;I loved how kind and helpful the instructors were. Even as a beginner, I felt included and encouraged.&quot;
+                        </p>
+                    </div>
+                </div>
+                <div className="grid grid-cols-4">
+                <Image src="/image_1.png" width={100} height={100} alt="Teams" />
+            </div>
+            </div>
+            
+      </div>
+  )
+}
